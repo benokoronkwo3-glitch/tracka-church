e4acf13765389a4edd37b3da08ab0e17e3d51865
benokoronkwo3-glitch <benokoronkwo3@gmail.com>
2026-06-30 09:04:56 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,35 @@
 
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { error: null }
+  }
+  static getDerivedStateFromError(error) {
+    return { error }
+  }
+  render() {
+    if (this.state.error) {
+      return (
+        <div style={{ padding: 30, fontFamily: 'monospace', background: '#fff', color: 'red', whiteSpace: 'pre-wrap', fontSize: 13 }}>
+          <h2>ERROR FOUND:</h2>
+          {this.state.error.toString()}
+          <br /><br />
+          {this.state.error.stack}
+        </div>
+      )
+    }
+    return this.props.children
+  }
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
+)
